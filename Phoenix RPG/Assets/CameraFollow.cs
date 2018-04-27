da42b6cf657b1500dffe8bd7c77500e533b6ad47
Alexander Hartson <alexander-hartson@live.com>
2018-04-26 21:45:58 -0500

    adding code

---
+++ Phoenix RPG/Assets/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        print(player.ToString());
+    }
+
+    // Update is called once per frame
+    void LateUpdate() {
+        // Camera Movement
+
+        // If you dont' want the camera to have a smooth panning / turning one,
+        //then just replace the camera look at code with:
+        //  transform.lookt(CameraGoalLookAt);
+    }
+}
